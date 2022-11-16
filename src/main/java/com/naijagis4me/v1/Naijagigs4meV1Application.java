6b65af8ca638360ac4775b26a844a7ace9f57825
David Baba <davidbabasalifu2020.com>
2022-11-16 08:48:58 +0100

    Initial commit

---
+++ src/main/java/com/naijagis4me/v1/Naijagigs4meV1Application.java
@@ -0,0 +1,13 @@
+package com.naijagis4me.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Naijagigs4meV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Naijagigs4meV1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/naijagis4me/v1/models/Users.java
@@ -0,0 +1,6 @@
+package com.naijagis4me.v1.models;
+
+public class Users {
+    private Long id;
+    private String name;
+}
